f31b37b2e93aa3f7ceeeaf80fcb8d001530e78cb
Jonas00016 <65242536+Jonas00016@users.noreply.github.com>
2023-03-05 22:45:22 +0100

    added index.ja

---
+++ index.js
@@ -0,0 +1,17 @@
+const data = {
+    weight: 70,
+    drinks: [
+        {
+            type: 'beer',
+            amount: 1,
+        },
+        {
+            type: 'wine',
+            amount: 1,
+        },
+    ],
+}
+
+// Implement me
+
+console.log(decideOnDrivingAbility(data))
